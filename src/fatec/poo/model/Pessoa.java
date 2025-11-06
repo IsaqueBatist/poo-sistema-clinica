bbf542702459ea92243ff53516961109536648e9
VictorLeonardo1346 <victorleosilva18@gmail.com>
2025-11-06 13:56:27 -0300

    Classe Pessoa criada

---
+++ src/fatec/poo/model/Pessoa.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fatec.poo.model;
+
+/**
+ *
+ * @author isaque
+ * @author Classe Pessoa: Victor Leonardo
+ */
+public abstract class Pessoa {
+
+    private String cpf;
+    private String nome;
+    private String endereco;
+    private String telefone;
+
+    public Pessoa(String cpf, String nome) {
+        this.cpf = cpf;     
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return this.cpf;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getEndereco() {
+        return this.endereco;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getTelefone() {
+        return this.telefone;
+    }
+}
