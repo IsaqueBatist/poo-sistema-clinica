1643d8de7b5ed5674eeff31ed8df91bd2c22441c
bibiritriz <ntenhodoritosmasdepende@gmail.com>
2025-11-19 18:56:34 -0300

    :sparkles: feat: Adicionando a classe PreparaConexao

---
+++ src/fatec/poo/control/PreparaConexao.java
@@ -0,0 +1,55 @@
+package fatec.poo.control;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author Beatriz-Camargo
+ */
+public class PreparaConexao {
+    private String connectionString;
+   private String driver;
+   private String usuario;
+   private String senha;
+   private Connection connection = null;
+
+   public PreparaConexao(String usuario, String senha) {   
+        this.usuario = usuario;
+        this.senha = senha;   
+   }
+
+   public void setConnectionString(String connectionString) {
+        this.connectionString = connectionString;
+   }
+
+   public void setDriver(String driver) {
+        this.driver = driver;
+   }
+   
+   public Connection abrirConexao() {       
+        if (connection == null){
+	    try {
+                  Class.forName(driver);
+            
+                  connection = DriverManager.getConnection(connectionString, usuario, senha);               
+	          System.out.println("[Conexão OK]");
+            }catch (Exception ex) {
+                  System.out.println("Falha na Abertura da Conexão");
+                  System.out.println(ex.toString() + ex.getMessage());
+            }
+	}
+       
+	return connection;
+   }
+   
+   public void fecharConexao(){        
+	try {
+              connection.close();
+              System.out.println("[Conexão Encerrada]");
+        }catch (Exception ex) {
+            System.out.println("Falha no Encerramento da Conexão");
+            System.out.println(ex.toString() + ex.getMessage());    
+        }
+    }
+}
